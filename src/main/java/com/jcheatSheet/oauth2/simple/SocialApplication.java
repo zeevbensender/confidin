3e17846be96d730dfb90582bfa3bcacc872bce20
Ben Sender <zeevb@hpe.com>
2017-04-13 19:38:47 +0300

    initial commit. missing properties

---
+++ src/main/java/com/jcheatSheet/oauth2/simple/SocialApplication.java
@@ -0,0 +1,37 @@
+package com.jcheatSheet.oauth2.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().
+				csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+}
