4cf30e9685542c390b0f28bca1258ff703bf96e9
Ben Sender <zeevb@hpe.com>
2017-04-14 05:48:10 +0300

    unprotected working application. available apis: http://localhost:8080/public/user http://localhost:8080/public/user/username

---
+++ src/main/java/com/confidin/Application.java
@@ -0,0 +1,26 @@
+package com.confidin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@SpringBootApplication
+public class Application extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/public/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().
+				csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/com/confidin/model/UserProfile.java
@@ -0,0 +1,33 @@
+package com.confidin.model;
+
+/**
+ * Created by bensende on 14/04/2017.
+ */
+public class UserProfile {
+    private String name;
+    private String email;
+
+    public UserProfile(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserProfile() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
